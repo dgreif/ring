3fe39cd8106da178b8003bc55b50141c00e07fe7
dgreif <dustin.greif@gmail.com>
2019-06-21 20:52:20 -0700

    fix(homebridge): ignore requests for live stream

--- homebridge/camera-source.ts
+++ homebridge/camera-source.ts
@@ -2,52 +2,33 @@ import { RingCamera } from '../api'
 import { hap, HAP } from './hap'
 import Service = HAP.Service
 
-// This is a work in progress. Still need to implement stream prep and start
+// SOMEDAY: implement stream prepareStream and handleStreamRequest
 export class CameraSource {
   services: Service[] = []
   streamControllers: any[] = []
 
   constructor(private ringCamera: RingCamera) {
     let options = {
-      proxy: false, // Requires RTP/RTCP MUX Proxy
-      disable_audio_proxy: false, // If proxy = true, you can opt out audio proxy via this
-      srtp: true, // Supports SRTP AES_CM_128_HMAC_SHA1_80 encryption
       video: {
-        resolutions: [
-          [1920, 1080, 30], // Width, Height, framerate
-          [320, 240, 15], // Apple Watch requires this configuration
-          [1280, 960, 30],
-          [1280, 720, 30],
-          [1024, 768, 30],
-          [640, 480, 30],
-          [640, 360, 30],
-          [480, 360, 30],
-          [480, 270, 30],
-          [320, 240, 30],
-          [320, 180, 30]
-        ],
+        resolutions: [],
         codec: {
-          profiles: [0, 1, 2], // Enum, please refer StreamController.VideoCodecParamProfileIDTypes
-          levels: [0, 1, 2] // Enum, please refer StreamController.VideoCodecParamLevelTypes
+          profiles: [],
+          levels: []
         }
       },
       audio: {
-        comfort_noise: false,
-        codecs: [
-          {
-            type: 'OPUS', // Audio Codec
-            samplerate: 24 // 8, 16, 24 KHz
-          },
-          {
-            type: 'AAC-eld',
-            samplerate: 16
-          }
-        ]
+        codecs: []
       }
     }
 
-    this.createCameraControlService()
-    this.createStreamControllers(2, options)
+    this.services.push(new hap.Service.CameraControl())
+
+    for (let i = 0; i < 2; i++) {
+      const streamController = new hap.StreamController(i, options, this)
+
+      this.services.push(streamController.service)
+      this.streamControllers.push(streamController)
+    }
   }
 
   async handleSnapshotRequest(
@@ -56,6 +37,8 @@ export class CameraSource {
   ) {
     try {
       const snapshot = await this.ringCamera.getSnapshot()
+      // Not currently resizing the image.
+      // HomeKit does a good job of resizing and doesn't seem to care if it's not right
       callback(undefined, snapshot)
     } catch (e) {
       callback(e)
@@ -65,24 +48,8 @@ export class CameraSource {
   handleCloseConnection(connectionID: any) {}
 
   prepareStream(request: any, callback: (response: any) => void) {
-    callback(new Error('Not implemented'))
+    // intentionally ignore the request.  This prevents "No Response" overlay for about 20s
   }
 
   handleStreamRequest(request: any) {}
-
-  private createCameraControlService() {
-    let controlService = new hap.Service.CameraControl()
-    this.services.push(controlService)
-  }
-
-  private createStreamControllers(maxStreams: number, options: any) {
-    let self = this
-
-    for (var i = 0; i < maxStreams; i++) {
-      var streamController = new hap.StreamController(i, options, self)
-
-      self.services.push(streamController.service)
-      self.streamControllers.push(streamController)
-    }
-  }
 }

2c740f51cc12f1df299b011c06c8a34fe03dffcb
dgreif <dustin.greif@gmail.com>
2020-01-21 08:16:47 -0700

    chore: switch from mocha to jest

--- test/api/ring-camera.spec.ts
+++ test/api/ring-camera.spec.ts
@@ -1,32 +1,31 @@
 import { getBatteryLevel } from '../../api'
-import { expect } from 'chai'
 
 describe('Ring Camera', () => {
   describe('battery level', () => {
     it('should handle string battery life', () => {
-      expect(getBatteryLevel({ battery_life: '49' })).to.equal(49)
+      expect(getBatteryLevel({ battery_life: '49' })).toEqual(49)
     })
 
     it('should handle null battery life', () => {
-      expect(getBatteryLevel({ battery_life: null })).to.equal(null)
+      expect(getBatteryLevel({ battery_life: null })).toEqual(null)
     })
 
     it('should handle right battery only', () => {
       expect(
         getBatteryLevel({ battery_life: null, battery_life_2: 24 })
-      ).to.equal(24)
+      ).toEqual(24)
     })
 
     it('should handle left battery only', () => {
       expect(
         getBatteryLevel({ battery_life: 76, battery_life_2: null })
-      ).to.equal(76)
+      ).toEqual(76)
     })
 
     it('should handle dual batteries', () => {
       expect(
         getBatteryLevel({ battery_life: '92', battery_life_2: 84 })
-      ).to.equal((84 + 92) / 2)
+      ).toEqual((84 + 92) / 2)
     })
   })
 })

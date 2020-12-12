c461f9a8709a3e9019bcedf4701447296ef1e1ff
dgreif <dustin.greif@gmail.com>
2020-12-12 12:33:38 -0700

    chore: update prod dependencies

--- api/util.ts
+++ api/util.ts
@@ -2,7 +2,7 @@ import debug = require('debug')
 import { red } from 'colors'
 import { createInterface } from 'readline'
 import { v4 as generateRandomUuid, v5 as generateUuidFromNamespace } from 'uuid'
-import { machineId } from 'node-machine-id'
+import { uuid as getSystemUuid } from 'systeminformation'
 
 const debugLogger = debug('ring'),
   uuidNamespace = 'e53ffdc0-e91d-4ce1-bec2-df939d94739c'
@@ -59,12 +59,22 @@ export function generateUuid(seed?: string) {
 }
 
 export async function getHardwareId() {
-  const id = await Promise.race([machineId(), delay(5000).then(() => '')])
+  const timeoutValue = '-1',
+    { os: id } = await Promise.race([
+      getSystemUuid(),
+      delay(5000).then(() => ({ os: timeoutValue })),
+    ])
 
-  if (!id) {
+  if (id === timeoutValue) {
     logError(
-      'Request for machine id timed out.  Falling back to random session id'
+      'Request for system uuid timed out.  Falling back to random session id'
     )
+    return generateRandomUuid()
+  }
+
+  if (id === '-') {
+    // default value set by systeminformation if it can't find a real value
+    logError('Unable to get system uuid.  Falling back to random session id')
     return generateRandomUuid()
   }
 

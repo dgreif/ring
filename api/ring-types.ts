c7a7e9cabf45ec6579a7d9a1033fa01930923243
dgreif <dustin.greif@gmail.com>
2022-05-07 13:34:33 -0700

    feat: add `cocoa_floodlight`
    Closes #893

--- api/ring-types.ts
+++ api/ring-types.ts
@@ -91,6 +91,7 @@ export enum RingCameraKind {
   floodlight_pro = 'floodlight_pro',
   cocoa_camera = 'cocoa_camera', // appears to be used for all next gen stickup cams (wired/battery/solar)
   cocoa_doorbell = 'cocoa_doorbell',
+  cocoa_floodlight = 'cocoa_floodlight',
   stickup_cam_mini = 'stickup_cam_mini',
 }
 
@@ -120,6 +121,7 @@ export const RingCameraModel: { readonly [P in RingCameraKind]: string } = {
   floodlight_pro: 'Floodlight Pro',
   cocoa_camera: 'Stick Up Cam',
   cocoa_doorbell: 'Doorbell Gen 2',
+  cocoa_floodlight: 'Floodlight Cam Plus',
   stickup_cam_mini: 'Indoor Cam',
 }
 

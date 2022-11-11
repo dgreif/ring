db975cfbb6f210f1aa76f63c37d3e55f75e7d894
dgreif <dustin.greif@gmail.com>
2022-11-11 14:53:09 -0700

    Fix lint

--- packages/ring-client-api/ring-auth-cli.ts
+++ packages/ring-client-api/ring-auth-cli.ts
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 import { logRefreshToken } from './refresh-token'
-logRefreshToken()
+logRefreshToken().catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error(e)
+  process.exit(1)
+})
--- packages/ring-client-api/ring-device-data-cli.ts
+++ packages/ring-client-api/ring-device-data-cli.ts
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 import { logDeviceData } from './device-data'
-logDeviceData()
+logDeviceData().catch((e) => {
+  // eslint-disable-next-line no-console
+  console.error(e)
+  process.exit(1)
+})

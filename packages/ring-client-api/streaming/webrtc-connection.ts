2a5f1b7658b115e480dc79877d6c23bee32c609d
dgreif <dustin.greif@gmail.com>
2023-08-05 15:14:21 -0700

    Move streaming message types to a separate file

--- packages/ring-client-api/streaming/webrtc-connection.ts
+++ packages/ring-client-api/streaming/webrtc-connection.ts
@@ -13,77 +13,7 @@ import crypto from 'crypto'
 import { BasicPeerConnection, WeriftPeerConnection } from './peer-connection'
 import { Subscribed } from '../subscribed'
 import { RtpPacket } from 'werift'
-
-interface SessionBody {
-  doorbot_id: number
-  session_id: string
-}
-
-interface AnswerMessage {
-  method: 'sdp'
-  body: {
-    sdp: string
-    type: 'answer'
-  } & SessionBody
-}
-
-interface IceCandidateMessage {
-  method: 'ice'
-  body: {
-    ice: string
-    mlineindex: number
-  } & SessionBody
-}
-
-interface SessionCreatedMessage {
-  method: 'session_created'
-  body: SessionBody
-}
-
-interface SessionStartedMessage {
-  method: 'session_started'
-  body: SessionBody
-}
-
-interface PongMessage {
-  method: 'pong'
-  body: SessionBody
-}
-
-interface NotificationMessage {
-  method: 'notification'
-  body: {
-    is_ok: boolean
-    text: string
-  } & SessionBody
-}
-
-// eslint-disable-next-line no-shadow
-enum CloseReasonCode {
-  NormalClose = 0,
-  // reason: { code: 5, text: '[rsl-apps/webrtc-liveview-server/Session.cpp:429] [Auth] [0xd540]: [rsl-apps/session-manager/Manager.cpp:227] [AppAuth] Unauthorized: invalid or expired token' }
-  // reason: { code: 5, text: 'Authentication failed: -1' }
-  // reason: { code: 5, text: 'Sessions with the provided ID not found' }
-  AuthenticationFailed = 5,
-  // reason: { code: 6, text: 'Timeout waiting for ping' }
-  Timeout = 6,
-}
-
-interface CloseMessage {
-  method: 'close'
-  body: {
-    reason: { code: CloseReasonCode; text: string }
-  } & SessionBody
-}
-
-type IncomingMessage =
-  | AnswerMessage
-  | IceCandidateMessage
-  | SessionCreatedMessage
-  | SessionStartedMessage
-  | PongMessage
-  | CloseMessage
-  | NotificationMessage
+import { IncomingMessage } from './streaming-messages'
 
 export interface StreamingConnectionOptions {
   createPeerConnection?: () => BasicPeerConnection

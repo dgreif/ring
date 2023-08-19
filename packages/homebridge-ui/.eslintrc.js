8bea97260009e93cc321f0f45347368c3cef663a
dgreif <dustin.greif@gmail.com>
2023-08-19 15:10:15 -0700

    Remove react-based homebridge-ui package

--- packages/homebridge-ui/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-  },
-  extends: ['shared'],
-}
--- packages/homebridge-ui/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom'
-import CustomConfigUi from './CustomConfigUi'
-import '@homebridge/plugin-ui-utils/dist/ui.interface'
-
-window.homebridge.addEventListener('ready', () => {
-  ReactDOM.render(<CustomConfigUi />, document.getElementById('root'))
-})
